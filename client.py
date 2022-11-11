05e0c76ee390771b3c0031582143c0a600285d61
Danylo77 <dankoiniesta@gmail.com>
2022-11-11 18:05:15 +0200

    er:GET

--- client.py
+++ client.py
@@ -1,33 +1,40 @@
 import requests
 import json
 import sys
+from flask import Flask, render_template
 
 test_data = {
     'test_data_first_name': 'Abc',
     'test_data_second_name': 'Zxc'
 }
-print(len(sys.argv))
-port = sys.argv[0]
 
-r = requests.post('http://127.0.0.1:' + port, data=json.dumps(test_data))
+# port = sys.argv[1]
+# print(port)
+#
+# r = requests.post('http://127.0.0.1:' + port, data=json.dumps(test_data))
 
-print(r.json())
 
-class Clien:
-    def __init__(self, adress):
-        self.adress = adress
+class Client:
+    app = Flask(name)
+
+    @app.route("/", methods = ["GET"])
+    def write_message(self):
+        return "<h1>HELLO</h1>"
+
+    def init(self, address: str):
+        self.address = address
         self.max_node = None
         self.min_node = None
         self.number_of_tries = 100
         self.current_try_number = 0
 
     def read(self):
-        self.__update_stats()
-        return json.loads(requests.get(self.max_node + '/read_message').tex)
+        self.__update_stats_()
+        return json.loads(requests.get(self.max_node + '/read_message').text)
 
-    def write(self):
-        self.__update_stats()
-        requests.post(self.min_node+'/write_message', data=json.dumps(message))
+    def write(self, message):
+        self.__update_stats_()
+        requests.post(self.min_node + '/write_message', data=json.dumps(message))
 
     def check(self):
         pass
@@ -38,12 +45,21 @@ class Clien:
     def delete_node(self):
         pass
 
-    def __update_stats(self):
+    def __update_stats_(self):
         if self.current_try_number == 0:
-            temp = requests.get(self.adress + '/get_stats').text
+            temp = requests.get(self.address + '/get_stats').text
             stats = json.loads(temp)
             self.max_node = max(stats, key=lambda x: stats[x])
             self.min_node = min(stats, key=lambda x: stats[x])
 
         self.current_try_number = (self.current_try_number + 1) % self.number_of_tries
 
+
+
+
+
+client_1 = Client("http://127.0.0.1:8011")
+
+
+client_1.write(test_data)
+print(client_1.read())
--- server.py
+++ server.py
@@ -2,33 +2,42 @@ from http import server
 import json
 from multiprocessing import Process
 
+HOST = "127.0.0.1"
+PORT = "8011"
+
+
 class Handler(server.BaseHTTPRequestHandler):
     def do_POST(self):
         self.send_response(200)
         self.end_headers()
         body_length = int(self.headers['content-length'])
         request_body_json_string = self.rfile.read(body_length).decode('utf-8')
-        
+
         # Printing  some info to the server console
         print('Server on port ' + str(self.server.server_port) + ' - request body: ' + request_body_json_string)
-        
+
         json_data_obj = json.loads(request_body_json_string)
         json_data_obj['SEEN_BY_THE_SERVER'] = 'Yes'
-        
+
         # Sending data to the client
         self.wfile.write(bytes(json.dumps(json_data_obj), 'utf-8'))
+
 
 def start_server(server_address):
     my_server = server.ThreadingHTTPServer(server_address, Handler)
     print(str(server_address) + ' Waiting for POST requests...')
     my_server.serve_forever()
 
-def start_local_server_on_port(port):
-    p = Process(target=start_server, args=(('127.0.0.1', port),))
-    p.start()
-def test():
-    pass
 
-if(__name__ == '__main__'):
-    start_local_server_on_port(8011)
-    start_local_server_on_port(8012)
+def start_local_server_on_port(port):
+    p = Process(target=start_server, args=((HOST, port),))
+    p.start()
+
+
+# port_one = 8011
+port_two = 8012
+
+
+if __name__ == 'main':
+    start_local_server_on_port(int(PORT))
+    start_local_server_on_port(port_two)
